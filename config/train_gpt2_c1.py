964f3ecca484f9ba85081217b484e41e4ad86e2e
ianmmeirelles@outlook.com <ianmmeirelles@outlook.com>
2025-06-01 16:15:18 +0200

    add new train

--- config/train_gpt2_c1.py
+++ config/train_gpt2_c1.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-gpt0-c0'
+out_dir = 'out-gpt2-c1'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
@@ -14,13 +14,13 @@ gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 32 # context of up to 256 previous characters
 
-n_layer = 2
-n_head = 2
+n_layer = 9
+n_head = 8
 n_embd = 96
 dropout = 0.0
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 20957
+max_iters = 24033
 lr_decay_iters = max_iters # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
